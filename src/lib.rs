76cd1f494a44387ee3a10ec13d2e0c9144270e7a
Lukas Germerott <lukas@lukasgermerott.de>
2024-08-01 19:36:45 +0200

    color extraction

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,28 @@ mod utils;
 
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
-}
+use image::EncodableLayout;
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, wasm-image-color-abstraction!");
+pub async fn extract_color(url: &str) -> Result<JsValue, JsValue> {
+
+    let bytes = reqwest::get(url)
+        .await
+        .map_err(|e| JsValue::from_str(&format!("{:?}", e)))?
+        .bytes()
+        .await
+        .map_err(|e| JsValue::from_str(&format!("{:?}", e)))?
+        .to_vec();
+    let image = image::load_from_memory(&bytes).unwrap();
+
+
+
+    // if you are not sure
+    let colors = dominant_color::get_colors(image.to_rgb8().as_bytes(), false);
+
+    let js_array = js_sys::Array::new();
+    for color in colors {
+        js_array.push(&JsValue::from(color));
+    }
+    Ok(JsValue::from(js_array.slice(0, 3)))
 }
